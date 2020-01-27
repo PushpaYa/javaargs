e5971647a8cef0233e5608908947a7d9ed44b26c
PushpaYa <45025901+PushpaYa@users.noreply.github.com>
2020-01-27 11:07:11 +0530

    Update ArgsException.java

--- src/com/cleancoder/args/ArgsException.java
+++ src/com/cleancoder/args/ArgsException.java
@@ -3,53 +3,64 @@ package com.cleancoder.args;
 import static com.cleancoder.args.ArgsException.ErrorCode.*;
 
 public class ArgsException extends Exception {
-  private char errorArgumentId = '\0';
-  private String errorParameter = null;
-  private ErrorCode errorCode = OK;
+  private char errorArgumentId = '\0';	//Default value for error argument ID
+  private String errorParameter = null; //Default value for error parameter
+  private ErrorCode errorCode = OK;		//Default value for error code
 
   public ArgsException() {}
 
+  //Constructor with string type parameters
   public ArgsException(String message) {super(message);}
 
+  //Constructor with error code type parameters
   public ArgsException(ErrorCode errorCode) {
     this.errorCode = errorCode;
   }
 
+  //Constructor with error code and string type parameters
   public ArgsException(ErrorCode errorCode, String errorParameter) {
     this.errorCode = errorCode;
     this.errorParameter = errorParameter;
   }
 
+  //Constructor with error code, error argument ID and string type parameters
   public ArgsException(ErrorCode errorCode, char errorArgumentId, String errorParameter) {
     this.errorCode = errorCode;
     this.errorParameter = errorParameter;
     this.errorArgumentId = errorArgumentId;
   }
 
+  //Function to get the error argument ID
   public char getErrorArgumentId() {
     return errorArgumentId;
   }
 
+  //Function to set the error argument ID
   public void setErrorArgumentId(char errorArgumentId) {
     this.errorArgumentId = errorArgumentId;
   }
 
+  //Function to get the error parameter
   public String getErrorParameter() {
     return errorParameter;
   }
 
+  //Function to set the error parameter
   public void setErrorParameter(String errorParameter) {
     this.errorParameter = errorParameter;
   }
 
+  //Function to get the error code
   public ErrorCode getErrorCode() {
     return errorCode;
   }
 
+  //Function to set the error codes
   public void setErrorCode(ErrorCode errorCode) {
     this.errorCode = errorCode;
   }
 
+  //List of error messages
   public String errorMessage() {
     switch (errorCode) {
       case OK:
@@ -78,6 +89,7 @@ public class ArgsException extends Exception {
     return "";
   }
 
+  //List of error codes
   public enum ErrorCode {
     OK, INVALID_ARGUMENT_FORMAT, UNEXPECTED_ARGUMENT, INVALID_ARGUMENT_NAME,
     MISSING_STRING,
